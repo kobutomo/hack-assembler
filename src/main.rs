ecb93dcd964d1ab8fc0fe7986e3eec36f98136f6
kobutomo <n.tomoro13@gmail.com>
2022-02-13 21:34:21 +0900

    エラーハンドリング

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,21 @@ mod parser;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = env::args().collect::<Vec<String>>();
-    let path = &args[1];
+    let path = args.get(1);
+    if path == None {
+        eprintln!("need source file path");
+        return Ok(());
+    }
+    let path = path.unwrap();
+    // パース
     let instructions = parser::parse(io::BufReader::new(fs::File::open(path)?))?;
     for ins in instructions {
-        let decoded = decoder::decode(ins)?;
+        let decoded = decoder::decode(ins);
+        if let Err(e) = decoded {
+            eprintln!("{}", e);
+            return Ok(());
+        }
+        let decoded = decoded.unwrap();
         println!("{}", decoded);
     }
     Ok(())
