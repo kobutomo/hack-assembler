592a9ee1213d78e2232e28080f0a779dde1fcf97
kobutomo <n.tomoro13@gmail.com>
2022-02-17 00:26:00 +0900

    実装完了

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::io;
+use std::io::Write;
 
 mod decoder;
 mod parser;
@@ -15,9 +16,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     let path = path.unwrap();
     let mut symbol_table = symbol_table::new();
-    println!("{:?}", symbol_table);
     // パース
     let instructions = parser::parse(io::BufReader::new(fs::File::open(path)?), &mut symbol_table)?;
+    let mut writer = io::BufWriter::new(fs::File::create("out.hack")?);
     for ins in instructions {
         let decoded = decoder::decode(ins, &mut symbol_table);
         if let Err(e) = decoded {
@@ -25,7 +26,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             return Ok(());
         }
         let decoded = decoded.unwrap();
-        println!("{}", decoded);
+        writer.write((decoded + "\n").as_bytes())?;
     }
+    writer.flush()?;
     Ok(())
 }
